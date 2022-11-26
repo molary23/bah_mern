b99a0197aa1c7f5af2bbc6ff1d839662635dde1a
molary <molary23@gmail.com>
2022-11-25 21:48:11 -0500

    Verify JWT middleware created

---
+++ LoginService/router/middleware/verifyJWT.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const jwt = require("jsonwebtoken");
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization || req.headers.Authorization;
+  if (!authHeader) return res.sendStatus(401);
+  if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, decoded) => {
+    if (err) return res.sendStatus(403);
+    req.username = decoded.username;
+    next();
+  });
+};
+
+module.exports = verifyJWT;
--- LoginService/server.js
+++ LoginService/server.js
@@ -2,7 +2,8 @@
 
 const express = require("express"),
   cors = require("cors"),
-  corsOptions = require("./util/cors");
+  corsOptions = require("./util/cors"),
+  verifyJWT = require("./router/middleware/verifyJWT");
 require("dotenv").config();
 
 const app = express();
@@ -14,12 +15,18 @@ app.use(cors(corsOptions));
 require("./util/DBRelationships");
 
 // Import Apis
-const user = require("./router/api/user");
+
 const auth = require("./router/api/auth");
 
+const user = require("./router/api/user");
+
 // Use apis
-app.use("/api/user", user);
+// Apis that doesn't require JWT Authentication
 app.use("/api/auth", auth);
+
+app.use(verifyJWT);
+// Apis that require JWT Authentication
+app.use("/api/user", user);
 
 const port = process.env.PORT || 6000;
 
