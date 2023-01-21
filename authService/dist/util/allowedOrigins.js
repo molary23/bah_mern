2b50ff2b786d068cd72c1ce2459e782b41ada7b5
Hassan Azeez Adeola <mo_lary@yahoo.com>
2023-01-20 23:21:34 -0500

    rename folders

--- authService/dist/util/allowedOrigins.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.allowedOrigins = void 0;
-exports.allowedOrigins = [
-    "http://127.0.0.1",
-    "http://localhost",
-];
