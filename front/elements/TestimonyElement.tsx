12f25a1dc61c2548445f18c74e3ffc55f5ee39d9
Hassan Azeez Adeola <mo_lary@yahoo.com>
2023-04-30 22:30:19 -0400

    Meta of Robots added

--- front/elements/TestimonyElement.tsx
+++ front/elements/TestimonyElement.tsx
@@ -5,27 +5,25 @@ import { FaQuoteLeft } from "react-icons/fa";
 export default function TestimonyElement(props: TestimonyProps) {
   const { image, testifier, testimony } = props;
   return (
-    <div className="sm:basis-1/3">
-      <div className="w-10/12 mx-auto">
-        <div className="mb-4">
-          <p className="mb-4 text-justify">
-            <FaQuoteLeft />
-            <span>{testimony}</span>
-          </p>
+    <div className="w-10/12 mx-auto">
+      <div className="mb-4">
+        <p className="mb-4 text-justify">
+          <FaQuoteLeft />
+          <span>{testimony}</span>
+        </p>
+      </div>
+      <div className="testifier flex flex-col items-end">
+        <div className="mb-4 ">
+          <span className="">- {testifier}</span>
         </div>
-        <div className="flex-col">
-          <div className="mb-4">
-            <span className="">- {testifier}</span>
-          </div>
-          <div className="">
-            <Image
-              className="h-24 w-24 rounded-full"
-              src={image}
-              width={256}
-              height={256}
-              alt={`testimonial`}
-            />
-          </div>
+        <div className="">
+          <Image
+            className="h-24 w-24 rounded-full"
+            src={image}
+            width={256}
+            height={256}
+            alt={`testimonial`}
+          />
         </div>
       </div>
     </div>
--- front/layouts/Home/TestimonialSection.tsx
+++ front/layouts/Home/TestimonialSection.tsx
@@ -8,27 +8,33 @@ export default function TestimonialSection(props: RegularObject) {
     <section className="h-max py-24 bg-primary text-white px-4 sm:px-8 theme__section testimony__section">
       <IntroHeading heading="Testimonials" />
       <div className="flex flex-col gap-y-16 md:gap-x-8 sm:flex-row  ">
-        <TestimonyElement
-          testimony="BAH Engineering Consultant is responsive, committed, honest in all
+        <div className="sm:basis-1/3">
+          <TestimonyElement
+            testimony="BAH Engineering Consultant is responsive, committed, honest in all
             business transactions and competent in their operational and
             technical constructs. BAH Engineering Consultant takes pride in its
             business and show passions over profit."
-          testifier="Secured Records Management Solutions"
-          image={SITE_CONSTANTS.image + "sunset.jpeg"}
-        />
-        <TestimonyElement
-          testimony="BAH Engineering Consultant is responsive, committed, honest in all
+            testifier="Secured Records Management Solutions"
+            image={SITE_CONSTANTS.image + "srmsl.jpg"}
+          />
+        </div>
+        <div className="sm:basis-1/3">
+          <TestimonyElement
+            testimony="BAH Engineering Consultant has been a strong partner in our ability to deliver world-class material handling services to our clients. The company has delivered competencies in a specialty of forklift machinery, provided access to materials and mechanisms in warehousing and proven that collaboration is the real strength in business"
+            testifier="Baun Limited"
+            image={SITE_CONSTANTS.image + "baun.jpg"}
+          />
+        </div>
+        <div className="sm:basis-1/3">
+          <TestimonyElement
+            testimony="BAH Engineering Consultant is responsive, committed, honest in all
             business transactions and competent in their operational and
             technical constructs. BAH Engineering Consultant takes pride in its
             business and show passions over profit."
-          testifier="Secured Records Management Solutions"
-          image={SITE_CONSTANTS.image + "sunset.jpeg"}
-        />
-        <TestimonyElement
-          testimony="BAH Engineering Consultant has been a strong partner in our ability to deliver world-class material handling services to our clients. The company has delivered competencies in a specialty of forklift machinery, provided access to materials and mechanisms in warehousing and proven that collaboration is the real strength in business"
-          testifier="Baun Limited"
-          image={SITE_CONSTANTS.image + "sunset.jpeg"}
-        />
+            testifier="Secured Records Management Solutions"
+            image={SITE_CONSTANTS.image + "srmsl.jpg"}
+          />
+        </div>
       </div>
     </section>
   );
