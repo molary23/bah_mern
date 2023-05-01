68b57aa7ee1025961f956e1c32e02ca400c26997
Hassan Azeez Adeola <mo_lary@yahoo.com>
2023-05-01 16:45:55 -0400

    subnav style changed

--- front/layouts/Nav.tsx
+++ front/layouts/Nav.tsx
@@ -177,7 +177,7 @@ const Nav = () => {
                 </Link>
 
                 <Link
-                  href="/showroom"
+                  href="/"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   eShowroom
